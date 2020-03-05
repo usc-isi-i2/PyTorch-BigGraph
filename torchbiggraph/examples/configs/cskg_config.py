46550366fc3c1b77b2b940c8227bb6acbe6b9801
filievski <filip.dbrsk@gmail.com>
2020-03-04 20:33:53 -0800

    cskg config and script - not tested

---
+++ torchbiggraph/examples/configs/cskg_config.py
@@ -0,0 +1,49 @@
+#!/usr/bin/env python3
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE.txt file in the root directory of this source tree.
+
+
+def get_torchbiggraph_config():
+
+    config = dict(
+        # I/O data
+        entity_path="data/cskg",
+        edge_paths=[
+            "data/cskg/train.csv",
+			"data/cskg/dev.csv",
+			"data/cskg/test.csv"
+        ],
+        checkpoint_path="model/cskg",
+
+        # Graph structure
+        entities={
+            'all': {'num_partitions': 1},
+        },
+        relations=[{
+            'name': 'all_edges',
+            'lhs': 'all',
+            'rhs': 'all',
+            'operator': 'complex_diagonal',
+        }],
+        dynamic_relations=True,
+
+        # Scoring model
+        dimension=400,
+        global_emb=False,
+        comparator='dot',
+
+        # Training
+        num_epochs=50,
+        num_uniform_negs=1000,
+        loss_fn='softmax',
+        lr=0.1,
+
+        # Evaluation during training
+        eval_fraction=0,  # to reproduce results, we need to use all training data
+    )
+
+    return config
